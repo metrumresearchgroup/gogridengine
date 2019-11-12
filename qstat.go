25f5c1b8953c1b0dbf53c0e1df19b3cfb018a9f6
darrellb <darrellb@metrumrg.com>
2019-11-12 12:57:57 -0500

    Working on using raw xml data for test input

--- qstat.go
+++ qstat.go
@@ -3,13 +3,10 @@ package gogridengine
 import (
 	"bytes"
 	"context"
-	"encoding/xml"
 	"errors"
-	"fmt"
-	"math/rand"
+	"io/ioutil"
 	"os"
 	"os/exec"
-	"strconv"
 	"strings"
 	"time"
 
@@ -174,138 +171,18 @@ func buildQstatArgumentList(filters map[string]string) []string {
 }
 
 func generatedQstatOputput() (string, error) {
+	file, err := os.Open("test_data/medium.xml")
 
-	entropy := rand.NewSource(time.Now().UnixNano())
-	random := rand.New(entropy)
-
-	ji := JobInfo{
-		XMLName: xml.Name{
-			Local: "job_info",
-		},
-		PendingJobs: PendingJob{
-			JobList: []Job{
-				{
-					XMLName: xml.Name{
-						Local: "job_list",
-					},
-					State:          "qw",
-					StateAttribute: "pending",
-					JBJobNumber:    1,
-					JATPriority:    random.Float64(),
-					JobName:        "Job-" + strconv.Itoa(random.Int()),
-					JobOwner:       "Owner-" + strconv.Itoa(random.Int()),
-					Slots:          3,
-				},
-			},
-		},
-		QueueInfo: QueueInfo{
-			XMLName: xml.Name{
-				Local: "queue_info",
-			},
-			Queues: []Host{
-				{
-					XMLName: xml.Name{
-						Local: "Queue-List",
-					},
-					Name:          "all.q@testing.local", //Always needs the @ symbol
-					SlotsTotal:    int32(random.Int()),
-					SlotsUsed:     int32(random.Int()),
-					SlotsReserved: int32(random.Int()),
-					LoadAverage:   float64(random.Float64()),
-					Resources: ResourceList{
-						{
-							Name:  "load_average",
-							Type:  "hl",
-							Value: "1.04",
-						},
-						{
-							Name:  "num_proc",
-							Type:  "ag",
-							Value: "3",
-						},
-						{
-							Name:  "mem_free",
-							Type:  "af",
-							Value: "2.04G",
-						},
-						{
-							Name:  "swap_free",
-							Type:  "ae",
-							Value: "500M",
-						},
-						{
-							Name:  "virtual_free",
-							Type:  "ad",
-							Value: "4G",
-						},
-						{
-							Name:  "mem_used",
-							Type:  "ac",
-							Value: "3G",
-						},
-						{
-							Name:  "mem_total",
-							Type:  "ab",
-							Value: "6G",
-						},
-						{
-							Name:  "cpu",
-							Type:  "aa",
-							Value: fmt.Sprintf("%f", random.Float64()),
-						},
-					},
-					JobList: []Job{
-						{
-							XMLName: xml.Name{
-								Local: "job_list",
-							},
-							State:          "r",
-							JBJobNumber:    2,
-							JATPriority:    random.Float64(),
-							StateAttribute: "running",
-							JobName:        "Job-" + strconv.Itoa(random.Int()),
-							JobOwner:       "Owner-" + strconv.Itoa(random.Int()),
-							Slots:          3,
-						},
-						{
-							XMLName: xml.Name{
-								Local: "job_list",
-							},
-							State:          "r",
-							JBJobNumber:    3,
-							JATPriority:    random.Float64(),
-							StateAttribute: "running",
-							JobName:        "validation",
-							JobOwner:       "Owner-" + strconv.Itoa(random.Int()),
-							Slots:          3,
-						},
-					},
-				},
-				{
-					XMLName: xml.Name{
-						Local: "Queue-List",
-					},
-					Name: "all.q@testing.second", //Always needs the @ symbol
-
-					Resources: ResourceList{},
-					JobList: []Job{
-						{
-							XMLName: xml.Name{
-								Local: "job_list",
-							},
-							State:          "r",
-							StateAttribute: "running",
-							JBJobNumber:    4,
-							JATPriority:    1,
-							JobName:        "Second-Host-Job",
-							JobOwner:       "Owner",
-							Slots:          14,
-						},
-					},
-				},
-			},
-		},
+	if err != nil {
+		return "", err
 	}
 
-	return ji.GetXML()
+	contents, err := ioutil.ReadAll(file)
+
+	if err != nil {
+		return "", err
+	}
+
+	return string(contents), nil
+
 }
