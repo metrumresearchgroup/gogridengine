d8859ac4826fe4413343ac47c02547714db8dbe5
spatilbroad <spatil@broadinstitute.org>
2024-03-19 14:50:50 -0400

    iterate

--- job.go
+++ job.go
@@ -38,7 +38,7 @@ func (t *Task) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
 
 	t.Source = v
 
-	if !strings.Contains(t.Source, ":") || (t.Source, ",") {
+	if !strings.Contains(t.Source, ":") || !strings.Contains(t.Source, ",") {
 		//Only process TaskIDs when not presented with a ":" or ","
 		parsed, err := strconv.ParseInt(t.Source, 10, 64)
 
